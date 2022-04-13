73e14aa066ea5451680d4c360bd5cafc1be0969f
Omar Bahlool <obahlool@ucsc.edu>
2022-04-12 20:08:51 -0700

    Secondary change

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -6,7 +6,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
         scene.add.existing(this);
  
         this.isFiring = false;
-        this.moveSpeed = 5;
+        this.moveSpeed =10;
         this.sfxRocket = scene.sound.add('sfx_rocket');
     }
 
