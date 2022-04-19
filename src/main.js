7ba4376b4c2f03e551cad64d52d193ea1ba6e682
Omar Bahlool <obahlool@ucsc.edu>
2022-04-19 02:03:33 -0700

    Comment

--- src/main.js
+++ src/main.js
@@ -14,3 +14,16 @@ let game = new Phaser.Game(config);
 // set UI sizes
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
+
+/*
+Name: Omar Bahlool
+Project name: Rocket Petrol: Medievel edition
+04/18/2022
+It took me approximetely 8-10 hours for to complete everything
+
+Point Breakdown:
+Add your own (copyright-free) background music to the Play scene (5)
+Allow the player to control the Rocket after it's fired (5)
+Implement a simultaneous two-player mode (30)
+Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi) (60)
+*/
